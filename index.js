a97ac39a4d9eb139ce44375876deaff90067263d
ProgrammerJM <johnmarktizado@gmail.com>
2022-03-21 02:28:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const path = require('path');
+const cors = require('cors')
+const PORT = process.env.PORT || 5050;
+
+<<<<<<< HEAD
+const whitelist = ['http://localhost:3000', 'http://localhost:5050', 'https://heroku123-app.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get("/", (req, res) => {
+    res.send("Server is running");
+  });
+  
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+  
+
+
+
+=======
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static('build'));
+  app.get('*',(req,res) => {
+    req.sendFile(path.resolve(__dirname,'build', 'index.html'))
+  })
+}
+
+>>>>>>> 1ce9e8965c40ca627b8f2c8ae9b09c635a4eb39b
+const users = {};
+
+const socketToRoom = {};
+
+//SOCKET CONFIGURATION
+
+io.on("connection", (socket) => {
+  // JOIN ROOM EVENT
+  socket.on("join room", (roomID) => {
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 10) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
+  // DISCONNECT EVENT
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+    socket.broadcast.emit("user left", socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
